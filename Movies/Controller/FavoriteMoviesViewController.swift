af07af40175779fe4abc390fa331ce9118934652
Danil Ryumin <ryumin_danil@icloud.com>
2022-07-25 17:39:55 +0300

    FavoriteMoviesViewController + FavoriteMovieCollectionViewCell

--- Movies/Controller/FavoriteMoviesViewController.swift
+++ Movies/Controller/FavoriteMoviesViewController.swift
@@ -1,10 +1,31 @@
 import UIKit
 
 class FavoriteMoviesViewController: UIViewController {
-
+    @IBOutlet weak var collectionView: UICollectionView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        collectionView.delegate = self
+        collectionView.dataSource = self
+        
+        let xibFavoriteCell = UINib(nibName: "FavoriteFilmViewCell", bundle: nil)
+        collectionView.register(xibFavoriteCell, forCellWithReuseIdentifier: "FavoriteFilmViewCell")
+        collectionView.reloadData()
+    }
+}
 
-        // Do any additional setup after loading the view.
+extension FavoriteMoviesViewController: UICollectionViewDelegate {
+    
+}
+
+extension FavoriteMoviesViewController: UICollectionViewDataSource {
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        <#code#>
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "FavoriteFilmViewCell", for: indexPath) as? FavoriteMovieCollectionViewCell else { return UICollectionViewCell() }
+        
+        return cell
     }
 }
---
+++ Movies/Model/FavoriteMovieCollectionViewCell.swift
@@ -0,0 +1,17 @@
+//
+//  FavoriteMovieCollectionViewCell.swift
+//  Movies
+//
+//  Created by Danil Ryumin on 25.07.2022.
+//
+
+import UIKit
+
+class FavoriteMovieCollectionViewCell: UICollectionViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+}
