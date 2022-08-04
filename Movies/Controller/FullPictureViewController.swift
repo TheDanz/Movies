82b7258d7c4ad5f8455685a741bb6576e661c760
Danil Ryumin <ryumin_danil@icloud.com>
2022-08-04 23:01:31 +0300

    FullPictureViewController

--- Movies/Controller/FullPictureViewController.swift
+++ Movies/Controller/FullPictureViewController.swift
@@ -1,15 +1,27 @@
-//
-//  FullPictureViewController.swift
-//  Movies
-//
-//  Created by Danil Ryumin on 24.06.2022.
-//
-
 import UIKit
 
 class FullPictureViewController: UIViewController {
-
+    @IBOutlet weak var collectionView: UICollectionView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        collectionView.delegate = self
+        collectionView.dataSource = self
+    }
+}
+
+extension FullPictureViewController: UICollectionViewDelegate {
+    
+}
+
+extension FullPictureViewController: UICollectionViewDataSource {
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 9
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "FullPictureCollectionViewCell", for: indexPath)
+        return cell
     }
 }
