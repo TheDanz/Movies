57c5af330704acf577b97cf9dc01311967093e33
Danil Ryumin <ryumin_danil@icloud.com>
2022-09-20 11:53:31 +0300

    New FullPictureVC

--- Movies/Controller/FullPictureViewController.swift
+++ Movies/Controller/FullPictureViewController.swift
@@ -2,7 +2,7 @@ import UIKit
 import RealmSwift
 
 class FullPictureViewController: UIViewController {
-    @IBOutlet weak var collectionView: UICollectionView!
+    @IBOutlet weak var imageView: UIImageView!
     @IBOutlet weak var numberOfPictureLabel: UILabel!
     var screenshotNumber: Int = Int()
     var receivedIndex: Int = Int()
@@ -12,38 +12,18 @@ class FullPictureViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-        collectionView.delegate = self
-        collectionView.dataSource = self
-        
+
         if let count = model.movieObjects?[receivedIndex].screenshots.count {
             numberOfPictureLabel.text = "\(screenshotNumber + 1) / \(count)"
-        }
-    }
-}
-
-extension FullPictureViewController: UICollectionViewDelegate {
-    
-}
-
-extension FullPictureViewController: UICollectionViewDataSource {
-    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        return 1
-    }
-    
-    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
-        guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "FullPictureCollectionViewCell", for: indexPath) as? FullPictureCollectionViewCell else {
-            return UICollectionViewCell()
         }
         
         guard let unwrFilmPic = model.movieObjects?[receivedIndex].screenshots[screenshotNumber],
               let posterURL = URL(string: address + unwrFilmPic) else {
-            return UICollectionViewCell()
+            return
         }
         
         service.getSetPoster(url: posterURL) { image in
-            cell.imageView.image = image
+            self.imageView.image = image
         }
-        return cell
     }
 }
--- Movies/Model/FullPictureCollectionViewCell.swift
+++
@@ -1,5 +0,0 @@
-import UIKit
-
-class FullPictureCollectionViewCell: UICollectionViewCell {
-    @IBOutlet weak var imageView: UIImageView!
-}
